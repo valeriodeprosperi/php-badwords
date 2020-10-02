0027082d6f80f0e2b12bf3e6a764806847619ace
valerio <valerio.deprosperi@gmail.com>
2020-10-02 15:52:42 +0200

    stampato paragrafo con php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$paragrafo = "Siete tutti sciocchi e maleducati e infami non potete fare così";
+$censura = 
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>php-badwords</title>
+   </head>
+   <body>
+     <p><?php echo $paragrafo;?></p>
+
+   </body>
+ </html>
